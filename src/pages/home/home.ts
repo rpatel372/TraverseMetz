22c6e8a1f4612712b78c5115c62b7981b5b0d5fe
rpatel372 <reemapatel97@gmail.com>
2018-03-11 20:15:44 -0200

    Added reverse geocoding - need to figure out how to display address in textfield

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { Geolocation ,GeolocationOptions ,Geoposition,PositionError } from '@ion
 import { AlertController } from 'ionic-angular';
 import { NativeGeocoder, NativeGeocoderReverseResult, NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
 
+
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -11,14 +12,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public alertCtrl: AlertController, private geolocation: Geolocation) {
+  constructor(public alertCtrl: AlertController, private geolocation: Geolocation, private nativeGeocoder: NativeGeocoder) {
 
   }
 
   testingThis(event, item) {
+  	//this.startLoc  = 'hello';
     this.geolocation.getCurrentPosition().then((resp) => {
-		 this.startLoc = resp.coords.latitude
+		 //this.startLoc = resp.coords.latitude;
 		 // resp.coords.longitude
+		 // console.log(resp.coords.latitude);
+		 // console.log(resp.coords.longitude);
+		 this.nativeGeocoder.reverseGeocode(resp.coords.latitude, resp.coords.longitude)
+		  .then((result: NativeGeocoderReverseResult) => console.log(JSON.stringify(result)))
+		  .catch((error: any) => console.log(error));
 		}).catch((error) => {
 		  console.log('Error getting location', error);
 		});
@@ -26,8 +33,13 @@ export class HomePage {
 	let watch = this.geolocation.watchPosition();
 	watch.subscribe((data) => {
 	 // data can be a set of coordinates, or an error (if an error occurred).
-	 this.startLoc = data.coords.latitude
+	 //this.startLoc = data.coords.latitude;
 	 // data.coords.longitude
+	 // console.log(data.coords.latitude);
+	 // console.log(data.coords.longitude);
+	 this.nativeGeocoder.reverseGeocode(data.coords.latitude, data.coords.longitude)
+		  .then((result: NativeGeocoderReverseResult) => console.log(JSON.stringify(result)))
+		  .catch((error: any) => console.log(error));
 	});
 	}
 
